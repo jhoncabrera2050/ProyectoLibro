1764c663bd462d25b1c990af157b4d941d6f8b6b
jhoncabrera <jhoncabrera2050@gmail.com>
2023-10-25 15:58:05 -0500

    Proyectoc#

---
+++ src/AppStore/Models/Domain/LibroCategoria.cs
@@ -0,0 +1,11 @@
+namespace AppStore.Models.Domain;
+
+
+
+public class LibroCategoria
+{
+    public int Id {get;set;}
+    public int CategoriaId {get;set;}
+
+    public int LibroId {get;set;}
+}
---
+++ src/AppStore/Models/Domain/Libros.cs
@@ -0,0 +1,14 @@
+namespace   AppStore.Models.Domain;
+
+
+public class Libro
+{
+    
+    public int Id {get;set;}
+    public string? Titulo {get;set;}
+    public string? CreateDate {get;set;}
+
+    public string? Imagen {get;set;}
+    public string? Author {get;set;}
+
+}
